73690f2c0821576701dd150e8ac5e51562e44f50
devGod <devgod@mail.ru>
2021-03-21 11:01:57 +0300

    Initial Commit

---
+++ MyName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyName
+//
+//  Created by Сергей Медведев on 21.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
